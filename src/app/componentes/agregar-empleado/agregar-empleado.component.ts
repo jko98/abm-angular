bd54d0949a78769a001adc7ac7c471f447e73b49
Joaquin Maidana <maidanajoaquin10@gmail.com>
2023-10-18 12:54:16 -0300

    agregando servicios

--- src/app/componentes/agregar-empleado/agregar-empleado.component.ts
+++ src/app/componentes/agregar-empleado/agregar-empleado.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FormGroup, FormBuilder} from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-agregar-empleado',
   templateUrl: './agregar-empleado.component.html',
   styleUrls: ['./agregar-empleado.component.css']
 })
-export class AgregarEmpleadoComponent {
+export class AgregarEmpleadoComponent implements OnInit{
+  formDeEmpleados:FormGroup;
 
+  constructor(public formulario:FormBuilder){
+  
+   this.formDeEmpleados=this.formulario.group({
+    nombre:[''],
+    correo:['']
+   })
+
+  }
+
+ngOnInit(): void {
+  
+}
+
+
+  enviarDato():any{
+  console.log("presionado"); 
+  console.log(this.formDeEmpleados.value);
+  }
 }
