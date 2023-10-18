a6307dc8d689588801aabcc39b936dca7467075c
Joaquin Maidana <maidanajoaquin10@gmail.com>
2023-10-18 13:09:48 -0300

    configurando el servicio

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable}  from 'rxjs';
+import { Empleado } from './empleado';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
+ 
+  api: string=''
 
-  constructor() { }
+  constructor() { 
+
+  }
 }
